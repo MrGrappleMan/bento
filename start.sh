c4bf2a60f76cc3a622b86f29c09d5c75f6beb41b
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2026-08-01 12:07:41 +0530

    Update comments

--- start.sh
+++ start.sh
@@ -2,14 +2,13 @@
 # set -e # Exit immediately if a command exits with a non-zero status
 clear
 
-# User reminder
-
+# Fail if not MacOS
 if [[ $(uname) != "Darwin" ]]; then
     echo "Environment is not macOS, try re-sshing into your macOS machine."
     exit 1
 fi
 
-# 1. Check and terminate if root
+# Fail if root
 if [ "$EUID" -eq 0 ]; then
     clear
     echo "Do not run this script directly as root or via sudo."
@@ -18,7 +17,7 @@ if [ "$EUID" -eq 0 ]; then
     exit 1
 fi
 
-# 2. Check SIP status using native Zsh syntax
+# Fail if SIP is disabled
 if csrutil status | grep -q "disabled"; then
     clear
     echo "Error: System Integrity Protection (SIP) is disabled."
@@ -28,9 +27,9 @@ if csrutil status | grep -q "disabled"; then
     echo
     echo "Enter Recovery Mode: Shut down your Mac, then press and hold the power button until the text, \"Loading startup options\" appears."
     echo "Access Terminal: Select Options > Continue, log in, and then navigate to Utilities > Terminal."
-    echo "Reset CSRutil: Type sudo csrutil reset, press Return, type Y to confirm, and enter your administrator password."
+    echo "Reset CSRutil: Type \"sudo csrutil reset\", press Return, type Y to confirm, and enter your administrator password."
     echo "Restart: Once the process completes, restart the machine for the changes to take effect."
-    echo "Verification: Log back in and run csrutil status in Terminal again to ensure SIP is enabled."
+    echo "Verification: Log back in and run \"csrutil status\" in Terminal again to ensure SIP is enabled."
     echo
     open https://www.youtube.com/watch?v=Fx_1OPFzu88&t=29s
     read -r -p "Press any key to quit..." -n1 -s
@@ -51,7 +50,7 @@ fi
 
 cd "$HOME"
 
-# 3. Install Homebrew (Non-Interactive)
+# Install Homebrew if not found (Non-Interactive)
 if ! command -v brew >/dev/null 2>&1; then
     echo "🍺 Installing Homebrew..."
     NONINTERACTIVE=1 bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -60,7 +59,7 @@ if ! command -v brew >/dev/null 2>&1; then
     eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
-# 4. Install Lix
+# Install Lix
 if ! command -v nix >/dev/null 2>&1; then
     echo "❄️ Installing Lix package manager..."
     curl --proto '=https' --tlsv1.2 -sSf -L https://install.lix.systems/lix | sh -s -- install --no-confirm
@@ -71,9 +70,9 @@ if ! command -v nix >/dev/null 2>&1; then
     fi
 fi
 
-# 5. Apply flake remotely
+# Apply flake from repo directly, use up to date files
 echo "🚀 Applying the configuration..."
 nix run nix-darwin -- switch --flake github:MrGrappleMan/bento#defaulthost --extra-experimental-features "nix-command flakes" --no-write-lock-file --refresh
 
-# 6. Reboot
+# Reboot
 sudo shutdown -r now
