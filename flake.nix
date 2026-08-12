73d5119527914916c9a22f400705f50bfb5460d7
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2026-08-12 20:10:22 +0530

    Add correct XDG portal

--- flake.nix
+++ flake.nix
@@ -75,6 +75,7 @@
         system = "x86_64-linux";
         modules = [
           home-manager.nixosModules.home-manager
+          /etc/nixos/hardware-configuration.nix
           ./linux/boot
           ./linux/flatpak
           ./linux/pkgs
